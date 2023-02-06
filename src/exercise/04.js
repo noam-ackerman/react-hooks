3736af70b71983dddfcc01fd535861af8f17a398
Noam Ackerman <developer.noam@gmail.com>
2023-02-06 16:54:11 +0100

    finishing exe 4 , 5 and half of 6

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,10 +2,19 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = useLocalStorageState('squares', Array(9).fill(null))
+   
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  React.useEffect(() => {
+     window.localStorage.setItem("squares", JSON.stringify(squares))
+  },[squares])
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -17,6 +26,10 @@ function Board() {
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
+
+    if (winner || squares[square]) {
+      return
+    }
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
@@ -26,7 +39,10 @@ function Board() {
     //
     // 🐨 make a copy of the squares array
     // 💰 `[...squares]` will do it!)
-    //
+    let squaresCopy = [...squares];
+    squaresCopy[square] = nextValue;
+    setSquares(squaresCopy)
+
     // 🐨 set the value of the square that was selected
     // 💰 `squaresCopy[square] = nextValue`
     //
@@ -36,6 +52,7 @@ function Board() {
   function restart() {
     // 🐨 reset the squares
     // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null));
   }
 
   function renderSquare(i) {
@@ -49,7 +66,7 @@ function Board() {
   return (
     <div>
       {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,32 +1,46 @@
 // useEffect: HTTP requests
 // http://localhost:3000/isolated/exercise/06.js
 
+
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {   
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonForm,
+  PokemonDataView, 
+} from '../pokemon'
+
+
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
-  // (This is to enable the loading state when switching between different pokemon.)
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [state, setState] = React.useState({status:'idle', pokemon: null, error:null})
 
-  // 💣 remove this
-  return 'TODO'
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return
+    }
+    setState({status:'pending', error: null})
+    fetchPokemon(pokemonName).then(pokemonData => {
+      setState({status:'resolved', pokemon: pokemonData});
+    }).catch(error => {
+      setState({status:'rejected',error: error});
+    })
+  }, [pokemonName])
+
+  if(state.status === 'idle'){
+    return "Submit a pokemon";
+  } else if (state.status === 'rejected') {
+    return (
+    <div role="alert">
+      There was an error: <pre style={{whiteSpace: 'normal'}}>{state.error.message}</pre>
+    </div>)
+  }  else if (state.status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else if (state.status === 'resolved'){
+   return <PokemonDataView pokemon={state.pokemon} />
+  }
+
+
 }
 
 function App() {
